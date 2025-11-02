d5f1495f11a49e305faeb2d7564ef1435ea10f02
brequet <requet.baptiste@gmail.com>
2025-11-02 20:05:01 +0100

    starship

--- common/config/pwsh7/profile.ps1
+++ common/config/pwsh7/profile.ps1
@@ -1,14 +1,5 @@
 Invoke-Expression (&starship init powershell)
 
-
-# $OmpTheme = "$HOME\.config\oh-my-posh\baba.omp.json"
-
-# if (Test-Path $OmpTheme) {
-#     oh-my-posh init pwsh --config $OmpTheme | Invoke-Expression
-# } else {
-#     Write-Warning "Oh-My-Posh theme not found at: $OmpTheme"
-# }
-
 function Update-EnvironmentVariables {
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 }
